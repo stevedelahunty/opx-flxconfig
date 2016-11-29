b3ae4ec567f0295e46bf2e6095f33243b532759b
Vikram Ragukumar <vragukumar@snaproute.com>
2016-11-29 00:19:36 -0800

    Add post update processing support to reconcile objects changes due to port breakout operation

---
+++ clients/asicdClient.go
@@ -0,0 +1,31 @@
+package clients
+
+import (
+	"asicdServices"
+	"fmt"
+	"models/objects"
+	"utils/dbutils"
+)
+
+const (
+	BREAKOUT_MODE_ATTR_OFFSET = 11
+)
+
+func (clnt *ASICDClient) PostUpdateProcessing(dbObj, obj objects.ConfigObj, attrSet []bool, dbHdl *dbutils.DBUtil) error {
+	if clnt.ClientHdl != nil {
+		if attrSet[BREAKOUT_MODE_ATTR_OFFSET] {
+			objList, err := clnt.ClientHdl.GetAllPortsWithDirtyCache()
+			if err != nil {
+				fmt.Println("Asicd post processing failed")
+				return err
+			}
+			for _, val := range objList {
+				var dbObj objects.Port
+				obj := (*asicdServices.Port)(val)
+				objects.ConvertThriftToasicdPortObj(obj, &dbObj)
+				dbHdl.StoreObjectInDb(dbObj)
+			}
+		}
+	}
+	return nil
+}
